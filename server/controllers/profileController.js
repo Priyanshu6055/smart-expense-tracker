95edf1ab49eb5d004ef9323fe556685939533e26
Priyanshu Raj <pkrr6055224@gmail.com>
2025-08-25 12:24:55 +0530

    forget password frontend added

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -112,21 +112,6 @@ exports.resetPassword = async (req, res) => {
 };
 
 
-exports.verifyOTP = async (req, res) => {
-  const { email, otp } = req.body;
-  const user = await User.findOne({ email });
-
-  if (!user) return res.status(404).json({ success: false, message: "User not found" });
-
-  if (user.resetOTP !== parseInt(otp) || user.resetOTPExpiry < Date.now()) {
-    return res.status(400).json({ success: false, message: "Invalid or expired OTP" });
-  }
-
-  res.json({ success: true, message: "OTP verified. You can reset password now." });
-};
-
-
-
 
 exports.logout = async (req, res) => {
   res.json({ success: true, message: "Logged out successfully" });
