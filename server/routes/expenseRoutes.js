6478c40455f5fc0d44c13bc76f23ff6d664686c8
Priyanshu Raj <128814954+Priyanshu6055@users.noreply.github.com>
2025-11-23 11:00:20 +0530

    Add route for getting monthly expense summary

--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -4,22 +4,21 @@ const {
   addTransaction,
   updateTransaction,
   deleteTransaction,
+  getMonthlySummary,
 } = require("../controllers/expenseController");
 
 const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-// GET all transactions for the logged-in user
 router.get("/", protect, getTransactions);
 
-// POST a new income or expense transaction
+router.get("/summary", protect, getMonthlySummary);
+
 router.post("/", protect, addTransaction);
 
-// PUT to update an existing transaction
 router.put("/:id", protect, updateTransaction);
 
-// DELETE a transaction by ID
 router.delete("/:id", protect, deleteTransaction);
 
 module.exports = router;
