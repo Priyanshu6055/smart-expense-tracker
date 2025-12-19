ae31cca0af6141a7aa2e47eda05411a5a52c5070
Priyanshu Raj <priyanshu6055@gmail.com>
2025-12-19 17:06:09 +0530

    new upi feature added

--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -5,8 +5,6 @@ const {
   updateTransaction,
   deleteTransaction,
   getMonthlySummary,
-  initiateUpiPayment,
-  confirmUpiPayment,
 } = require("../controllers/expenseController");
 
 const { protect } = require("../middlewares/authMiddleware");
@@ -14,15 +12,18 @@ const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.get("/", protect, getTransactions);
+
 router.get("/summary", protect, getMonthlySummary);
 
 router.post("/", protect, addTransaction);
 
-// 🔥 UPI ROUTES (ADD THESE)
+router.put("/:id", protect, updateTransaction);
+
+router.delete("/:id", protect, deleteTransaction);
+
 router.post("/upi/initiate", protect, initiateUpiPayment);
+
 router.patch("/upi/confirm/:id", protect, confirmUpiPayment);
 
-router.put("/:id", protect, updateTransaction);
-router.delete("/:id", protect, deleteTransaction);
 
 module.exports = router;
